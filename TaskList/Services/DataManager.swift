a22a02bfb86c9c5466a12c553734775382268233
bullinnyc <i8u11@icloud.com>
2021-05-24 22:02:51 +0700

    DataManger optimization

--- TaskList/Services/DataManager.swift
+++ TaskList/Services/DataManager.swift
@@ -20,24 +20,23 @@ class DataManager {
     
     // MARK: - Public Properties
     func createTempData(_ completion: @escaping () -> Void) {
-        if !UserDefaults.standard.bool(forKey: "done") {
+        if !UserDefaults.standard.bool(forKey: "DefaultTaskList") {
+            UserDefaults.standard.set(true, forKey: "DefaultTaskList")
             
-            UserDefaults.standard.set(true, forKey: "done")
-            
+            // Create TaskList
             let shoppingList = TaskList()
             shoppingList.name = "Shopping List"
             
-            let moviesList = TaskList(value: ["Movies List", Date(), [["Best film ever"], ["The best of the best", "Must have", Date(), true]]])
+            let moviesList = TaskList()
+            moviesList.name = "Movies List"
             
-            let milk = Task()
-            milk.name = "Milk"
-            milk.note = "2L"
-            
-            let bread = Task(value: ["Bread", "", Date(), true])
-            let apples = Task(value: ["name": "Apples", "note": "2Kg"])
+            // Create Task in TaskList
+            let milk = Task(value: ["name": "Milk", "note": "1L", "isComplete": true])
+            let bread = Task(value: ["name": "Bread", "note": "One"])
+            let apple = Task(value: ["name": "Apple", "note": "2Kg"])
             
             shoppingList.tasks.append(milk)
-            shoppingList.tasks.insert(contentsOf: [bread, apples], at: 1)
+            shoppingList.tasks.insert(contentsOf: [bread, apple], at: 1)
             
             DispatchQueue.main.async {
                 StorageManager.shared.save(taskLists: [shoppingList, moviesList])
